62117d6c97e76b8f78dca514275e88f94b691f0b
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-26 19:58:49 +0900

    Add: app/Main> writing eHandler -- implement face rotation manipulation

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Control.Concurrent (forkIO)
 import Control.Monad (forever)
 import Control.Lens (makeLenses)
 import FaceDataServer.Types (Radian, Percent)
+import FaceDataServer.Connection
 import Network.Multicast (multicastSender)
 
 data Name = NoName
@@ -24,10 +25,21 @@ portNum = 5032
 
 ui s = [vBox [str "Face-Data-Server easy controller"]]
 
+-- Event handler. Temporary code. NO COMMIT {{{
+update f l s = do
+    let s' = over f l s
+    sendFaceData (s^.sock) (s^.addr) s'
+    return s'
 
-
-eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'q') [])) = halt s
-eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'w') [])) = undefined
+eHandler s (VtyEvent (Vty.EvKey (Vty.KEsc) [])) = halt s
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'a') [])) = update (subtract 1/pi) (faceData&face_y_radian) s >>= continue
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 's') [])) = update (subtract 1/pi) (faceData&face_x_radian) s >>= continue
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'w') [])) = update (+ 1/pi) (faceData&face_x_radian) s >>= continue
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'd') [])) = update (+ 1/pi) (faceData&face_y_radian) s >>= continue
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'q') [])) = update (subtract 1/pi) (faceData&face_z_radian) s >>= continue
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'e') [])) = update (+ 1/pi) (faceData&face_z_radian) s >>= continue
+eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'd') [])) = undefined
+-- }}}
 
 app :: App AppState e Name
 app = App { appDraw         = ui
